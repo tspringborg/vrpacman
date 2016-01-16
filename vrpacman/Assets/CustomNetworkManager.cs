75715bee90e1f85e59d75569b028ee4063de074e
Thomas Springborg <thomas-springborg@hotmail.com>
2016-01-16 20:17:07 +0100

    new multiplayer test

---
+++ vrpacman/Assets/CustomNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CustomNetworkManager : NetworkManager {
+
+	[Header("Leave empty to use localhost")]
+	public string hostIP;
+
+	public void Awake(){
+		Debug.Log("hostIP:"+hostIP);
+		if(hostIP !=""){
+			networkAddress = hostIP;
+			StartClient();
+		}else{
+			StartHost();
+		}
+	}
+	public void Start(){
+		
+	}
+}

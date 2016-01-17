db73488c449cd48731c441106eee442687a8a6b1
Thomas Springborg <thomas-springborg@hotmail.com>
2016-01-17 12:26:41 +0100

    trying multiplayer stuff

--- vrpacman/Assets/Server.cs
+++ vrpacman/Assets/Server.cs
@@ -8,18 +8,10 @@ public class Server : NetworkManager {
 
 	public void Awake(){
 		StartHost();
-		autoCreatePlayer = true;
+
 	}
 	public void Start(){
 		SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
-	}
-	// called when a new player is added for a client
-	public virtual void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-	{
-		
-		//var player = (GameObject)GameObject.Instantiate(playerPrefab, SpawnLocations.ForNewPlayer(numPlayers), Quaternion.identity);
-		//NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-
-		base.OnServerAddPlayer(conn, playerControllerId);
+		autoCreatePlayer = true;
 	}
 }

47f0ae7c26b75f5550b184237efab664d1f6400b
Thomas Springborg <thomas-springborg@hotmail.com>
2016-01-17 11:31:16 +0100

    movement and server change

--- vrpacman/Assets/CustomNetworkManager.cs
+++ vrpacman/Assets/CustomNetworkManager.cs
@@ -11,10 +11,10 @@ public class CustomNetworkManager : NetworkManager {
 	public Transform playerViewport;
 	public Transform playerViewportRotate;
 
-	//private string hostIP = "10.40.0.170"; //ts
+	private string hostIP = "10.40.0.170"; //ts
 	//private string hostIP = "10.40.0.176"; //ns
 	//private string hostIP = "10.40.0.192"; //julie
-	private string hostIP = "10.40.1.51"; //samuel
+	//private string hostIP = "10.40.1.51"; //samuel
 
 	public void Awake(){
 		networkAddress = hostIP;

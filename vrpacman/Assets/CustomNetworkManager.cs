979b552738813ea0f1100c9e505ac54ef627ef91
Thomas Springborg <thomas-springborg@hotmail.com>
2016-01-17 04:55:16 +0100

    movement for gearvr player. Added an environment test scene

--- vrpacman/Assets/CustomNetworkManager.cs
+++ vrpacman/Assets/CustomNetworkManager.cs
@@ -2,16 +2,18 @@
 using UnityEngine.Networking;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Utility;
 
 
 
 public class CustomNetworkManager : NetworkManager {
-	
+
+	public Transform playerViewport;
 
 	//private string hostIP = "10.40.0.170"; //ts
-	//private string hostIP = "10.40.0.176"; //ns
+	private string hostIP = "10.40.0.176"; //ns
 	//private string hostIP = "10.40.0.192"; //julie
-	private string hostIP = "10.40.1.51"; //samuel
+	//private string hostIP = "10.40.1.51"; //samuel
 
 	public void Awake(){
 		networkAddress = hostIP;
@@ -24,6 +26,9 @@ public class CustomNetworkManager : NetworkManager {
 	{
 		base.OnClientConnect (conn);
 		SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
-
+		GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		FollowTarget followScript = playerViewport.gameObject.AddComponent<FollowTarget>();
+		followScript.offset = Vector3.zero;
+		followScript.target = player.transform;
 	}
 }
